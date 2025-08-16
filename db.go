f69b3245887d97d5d6811fa4a4bdb56720f4ed97
nikitairs <stlrvnk@gmail.com>
2025-08-16 15:00:53 +0300

    add db corruption validation

--- db.go
+++ db.go
@@ -1,38 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
 )
 
-const emptyDb string = `{}`
-
 func initialize() string {
 
-	var dbPath string = "database/database.json"
+	var dbName string = "database.json"
 
 	if len(os.Args) > 1 {
-		dbName := os.Args[1]
+		dbName = os.Args[1]
 
 		if !strings.HasSuffix(dbName, ".json") {
 			log.Fatalf("DB name must be a .json file, [%v] passed instead\n", dbName)
 		}
 
-		// check if file exists
-		dbPath = fmt.Sprintf("database/%v", dbName)
-		if _, err := os.Open(dbPath); err != nil {
-			fmt.Printf("DB [%v] does not exist, creating new\n", dbPath)
+		// check if db exists, create new if not
+		dbPath := fmt.Sprintf("database/%v", dbName)
+		file, err := os.Open(dbPath)
+		if err != nil {
+			fmt.Printf("DB [%v] does not exist, creating new...\n", dbPath)
 
-			err = os.WriteFile(dbPath, []byte(emptyDb), 0o644)
-			if err != nil {
+			os.Mkdir("database", 0o755)
+			if err = os.WriteFile(dbPath, []byte(`{}`), 0o644); err != nil {
 				log.Fatalf("Failed creating DB [%v] - %v\n", dbPath, err)
 			}
 		}
+
+		// check existing db for corruption
+		var sb strings.Builder
+
+		_, err = io.Copy(&sb, file)
+		if err != nil {
+			log.Fatalf("unable to retrieve data from file [%v]; file is corrupt!\n", dbName)
+		}
+
+		fileString := sb.String()
+		jsonMap := map[string]any{}
+
+		err = json.Unmarshal([]byte(fileString), &jsonMap)
+		if err != nil {
+			log.Fatalf("Unable to parse JSON - %v; file is corrupt\n", err)
+		}
+
 	} else {
 		log.Fatalln("Database name is not passed as argument")
 	}
-
-	return dbPath
+	return dbName
 }
